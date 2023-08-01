tomxi1997/libapparmor-dev_Android

commit 44f3be091a8dc19f3027547bd1fe091a34fb6a33

parser: convert the stored audit from a bit mask to a bool
This delays the convertion of the audit flag until passing to the
backend. This is a step towards fix the parser front end so that it
doesn't use encoded permission mappings.

Note: the patch embedds the bool conversion into a struct to ensure
the compiler will fail to build unless every use is fixed. The
struct is removed in the following patch.

Signed-off-by: John Johansen <john.johansen@canonical.com>

parser/af_unix.cc
@@ -105,7 +105,7 @@ unix_rule::unix_rule(unsigned int type_p, bool audit_p, bool denied):
 			yyerror("socket rule: invalid socket type '%d'", type_p);
 	}
 	perms = AA_VALID_NET_PERMS;
-	audit = audit_p ? AA_VALID_NET_PERMS : 0;
+	audit.audit = audit_p;
 	deny = denied;
 }
 
@@ -195,7 +195,7 @@ void unix_rule::downgrade_rule(Profile &prof) {
 		mask = 1 << sock_type_n;
 	if (!deny) {
 		prof.net.allow[AF_UNIX] |= mask;
-		if (audit)
+		if (audit.audit)
 			prof.net.audit[AF_UNIX] |= mask;
 	} else {
 		/* deny rules have to be dropped because the downgrade makes
@@ -336,7 +336,7 @@ int unix_rule::gen_policy_re(Profile &prof)
 		buf = buffer.str();
 		if (!prof.policy.rules->add_rule(buf.c_str(), deny,
 						 map_perms(AA_NET_CREATE),
-						 map_perms(audit & AA_NET_CREATE),
+						 map_perms(audit.audit ? AA_NET_CREATE : 0),
 						 dfaflags))
 			goto fail;
 		mask &= ~AA_NET_CREATE;
@@ -361,7 +361,7 @@ int unix_rule::gen_policy_re(Profile &prof)
 		buf = tmp.str();
 		if (!prof.policy.rules->add_rule(buf.c_str(), deny,
 						 map_perms(AA_NET_BIND),
-						 map_perms(audit & AA_NET_BIND),
+						 map_perms(audit.audit ? AA_NET_BIND : 0),
 						 dfaflags))
 			goto fail;
 		/* clear if auto, else generic need to generate addr below */
@@ -386,7 +386,7 @@ int unix_rule::gen_policy_re(Profile &prof)
 			buf = buffer.str();
 			if (!prof.policy.rules->add_rule(buf.c_str(), deny,
 							 map_perms(mask & local_mask),
-							 map_perms(audit & local_mask),
+							 map_perms(audit.audit ? mask & local_mask : 0),
 							 dfaflags))
 				goto fail;
 		}
@@ -400,7 +400,7 @@ int unix_rule::gen_policy_re(Profile &prof)
 			buf = tmp.str();
 			if (!prof.policy.rules->add_rule(buf.c_str(), deny,
 							 map_perms(AA_NET_LISTEN),
-							 map_perms(audit & AA_NET_LISTEN),
+							 map_perms(audit.audit ? AA_NET_LISTEN : 0),
 							 dfaflags))
 				goto fail;
 		}
@@ -412,8 +412,8 @@ int unix_rule::gen_policy_re(Profile &prof)
 			tmp << "..";
 			buf = tmp.str();
 			if (!prof.policy.rules->add_rule(buf.c_str(), deny,
-							 map_perms(mask & AA_NET_OPT),
-							 map_perms(audit & AA_NET_OPT),
+							 map_perms(AA_NET_OPT),
+							 map_perms(audit.audit ? AA_NET_OPT : 0),
 							 dfaflags))
 				goto fail;
 		}
@@ -432,7 +432,7 @@ int unix_rule::gen_policy_re(Profile &prof)
 			goto fail;
 
 		buf = buffer.str();
-		if (!prof.policy.rules->add_rule(buf.c_str(), deny, map_perms(perms & AA_PEER_NET_PERMS), map_perms(audit), dfaflags))
+		if (!prof.policy.rules->add_rule(buf.c_str(), deny, map_perms(perms & AA_PEER_NET_PERMS), map_perms(audit.audit ? perms & AA_PEER_NET_PERMS : 0), dfaflags))
 			goto fail;
 	}
 

parser/af_rule.cc
@@ -92,7 +92,7 @@ int af_rule::move_base_cond(struct cond_entry *ent, bool peer)
 
 ostream &af_rule::dump_prefix(ostream &os)
 {
-	if (audit)
+	if (audit.audit)
 		os << "audit ";
 	if (deny)
 		os << "deny ";

parser/af_rule.h
@@ -45,12 +45,12 @@ public:
 	char *label;
 	char *peer_label;
 	perms_t perms;
-	int audit;
+	struct { bool audit; } audit;
 	bool deny;
 
 	af_rule(const char *name): af_name(name), sock_type(NULL),
 		sock_type_n(-1), proto(NULL), proto_n(0), label(NULL),
-		peer_label(NULL), perms(0), audit(0), deny(0)
+		peer_label(NULL), perms(0), audit({ false }), deny(0)
 	{}
 
 	virtual ~af_rule()

parser/dbus.cc
@@ -69,7 +69,7 @@ void dbus_rule::move_conditionals(struct cond_entry *conds)
 dbus_rule::dbus_rule(perms_t perms_p, struct cond_entry *conds,
 		     struct cond_entry *peer_conds):
 	bus(NULL), name(NULL), peer_label(NULL), path(NULL), interface(NULL), member(NULL),
-	perms(0), audit(0), deny(0)
+	perms(0), audit( { false } ), deny(0)
 {
 	int name_is_subject_cond = 0, message_rule = 0, service_rule = 0;
 
@@ -122,7 +122,7 @@ dbus_rule::dbus_rule(perms_t perms_p, struct cond_entry *conds,
 
 ostream &dbus_rule::dump(ostream &os)
 {
-	if (audit)
+	if (audit.audit)
 		os << "audit ";
 	if (deny)
 		os << "deny ";
@@ -279,21 +279,21 @@ int dbus_rule::gen_policy_re(Profile &prof)
 
 	if (perms & AA_DBUS_BIND) {
 		if (!prof.policy.rules->add_rule_vec(deny, perms & AA_DBUS_BIND,
-						    audit & AA_DBUS_BIND,
+						     audit.audit ? perms & AA_DBUS_BIND : 0,
 						    2, vec, dfaflags, false))
 			goto fail;
 	}
 	if (perms & (AA_DBUS_SEND | AA_DBUS_RECEIVE)) {
 		if (!prof.policy.rules->add_rule_vec(deny,
 				       perms & (AA_DBUS_SEND | AA_DBUS_RECEIVE),
-				       audit & (AA_DBUS_SEND | AA_DBUS_RECEIVE),
+						     audit.audit ? perms & (AA_DBUS_SEND | AA_DBUS_RECEIVE) : 0,
 				       6, vec, dfaflags, false))
 			goto fail;
 	}
 	if (perms & AA_DBUS_EAVESDROP) {
 		if (!prof.policy.rules->add_rule_vec(deny,
 						    perms & AA_DBUS_EAVESDROP,
-						    audit & AA_DBUS_EAVESDROP,
+						     audit.audit ? perms & AA_DBUS_EAVESDROP : 0,
 						    1, vec, dfaflags, false))
 			goto fail;
 	}

parser/dbus.h
@@ -40,7 +40,7 @@ public:
 	char *interface;
 	char *member;
 	perms_t perms;
-	int audit;
+	struct { bool audit; } audit;
 	int deny;
 
 	dbus_rule(perms_t perms_p, struct cond_entry *conds,

parser/mount.cc
@@ -469,7 +469,7 @@ mnt_rule::mnt_rule(struct cond_entry *src_conds, char *device_p,
 		   struct cond_entry *dst_conds unused, char *mnt_point_p,
 		   perms_t perms_p):
 	mnt_point(mnt_point_p), device(device_p), trans(NULL), opts(NULL),
-	flagsv(0), opt_flagsv(0), audit(0), deny(0)
+	flagsv(0), opt_flagsv(0), audit( { false } ), deny(0)
 {
 	/* FIXME: dst_conds are ignored atm */
 	dev_type = extract_fstype(&src_conds);
@@ -581,7 +581,7 @@ ostream &mnt_rule::dump(ostream &os)
 		os << " -> " << trans;
 
 	const char *prefix = deny ? "deny" : "";
-	os << " " << prefix << "(0x" << hex << perms << "/0x" << audit << ")";
+	os << " " << prefix << "(0x" << hex << perms << "/0x" << (audit.audit ? perms : 0) << ")";
 	os << ",\n";
 
 	return os;
@@ -699,7 +699,6 @@ int mnt_rule::gen_policy_remount(Profile &prof, int &count,
 	std::string optsbuf;
 	char class_mount_hdr[64];
 	const char *vec[5];
-	perms_t tmpperms;
 
 	sprintf(class_mount_hdr, "\\x%02x", AA_CLASS_MOUNT);
 
@@ -727,14 +726,20 @@ int mnt_rule::gen_policy_remount(Profile &prof, int &count,
 
 	vec[3] = flagsbuf;
 
-	if (opts)
+	perms_t tmpperms, tmpaudit;
+	if (opts) {
 		tmpperms = AA_MATCH_CONT;
-	else
+		tmpaudit = 0;
+	} else {
+		/* dependent on full expansion of any data match perms */
 		tmpperms = perms;
-
-	/* rule for match without required data || data MATCH_CONT */
-	if (!prof.policy.rules->add_rule_vec(deny, tmpperms,
-					     audit | AA_AUDIT_MNT_DATA, 4,
+		tmpaudit = audit.audit ? perms : 0;
+	}
+	/* match for up to but not including data
+	 * if a data match is required this only has AA_MATCH_CONT perms
+	 * else it has full perms
+	 */
+	if (!prof.policy.rules->add_rule_vec(deny, tmpperms, tmpaudit, 4,
 					     vec, dfaflags, false))
 		goto fail;
 	count++;
@@ -746,7 +751,7 @@ int mnt_rule::gen_policy_remount(Profile &prof, int &count,
 			goto fail;
 		vec[4] = optsbuf.c_str();
 		if (!prof.policy.rules->add_rule_vec(deny, perms,
-						     audit | AA_AUDIT_MNT_DATA,
+						     (audit.audit ? perms : 0),
 						     5, vec, dfaflags, false))
 			goto fail;
 		count++;
@@ -787,7 +792,7 @@ int mnt_rule::gen_policy_bind_mount(Profile &prof, int &count,
 			     opt_flags & MS_BIND_FLAGS))
 		goto fail;
 	vec[3] = flagsbuf;
-	if (!prof.policy.rules->add_rule_vec(deny, perms, audit, 4, vec,
+	if (!prof.policy.rules->add_rule_vec(deny, perms, audit.audit ? perms : 0, 4, vec,
 					     dfaflags, false))
 		goto fail;
 	count++;
@@ -828,7 +833,7 @@ int mnt_rule::gen_policy_change_mount_type(Profile &prof, int &count,
 			     opt_flags & MS_MAKE_FLAGS))
 		goto fail;
 	vec[3] = flagsbuf;
-	if (!prof.policy.rules->add_rule_vec(deny, perms, audit, 4, vec,
+	if (!prof.policy.rules->add_rule_vec(deny, perms, audit.audit ? perms : 0, 4, vec,
 					     dfaflags, false))
 		goto fail;
 	count++;
@@ -870,7 +875,7 @@ int mnt_rule::gen_policy_move_mount(Profile &prof, int &count,
 			     opt_flags & MS_MOVE_FLAGS))
 		goto fail;
 	vec[3] = flagsbuf;
-	if (!prof.policy.rules->add_rule_vec(deny, perms, audit, 4, vec,
+	if (!prof.policy.rules->add_rule_vec(deny, perms, audit.audit ? perms : 0, 4, vec,
 					     dfaflags, false))
 		goto fail;
 	count++;
@@ -891,7 +896,6 @@ int mnt_rule::gen_policy_new_mount(Profile &prof, int &count,
 	std::string optsbuf;
 	char class_mount_hdr[64];
 	const char *vec[5];
-	perms_t tmpperms;
 
 	sprintf(class_mount_hdr, "\\x%02x", AA_CLASS_MOUNT);
 
@@ -913,14 +917,16 @@ int mnt_rule::gen_policy_new_mount(Profile &prof, int &count,
 		goto fail;
 	vec[3] = flagsbuf;
 
-	if (opts)
+	perms_t tmpperms, tmpaudit;
+	if (opts) {
 		tmpperms = AA_MATCH_CONT;
-	else
+		tmpaudit = 0;
+	} else {
 		tmpperms = perms;
-
+		tmpaudit = audit.audit ? perms : 0;
+	}
 	/* rule for match without required data || data MATCH_CONT */
-	if (!prof.policy.rules->add_rule_vec(deny, tmpperms,
-					     audit | AA_AUDIT_MNT_DATA, 4,
+	if (!prof.policy.rules->add_rule_vec(deny, tmpperms, tmpaudit, 4,
 					     vec, dfaflags, false))
 		goto fail;
 	count++;
@@ -932,7 +938,7 @@ int mnt_rule::gen_policy_new_mount(Profile &prof, int &count,
 			goto fail;
 		vec[4] = optsbuf.c_str();
 		if (!prof.policy.rules->add_rule_vec(deny, perms,
-						     audit | AA_AUDIT_MNT_DATA,
+						     audit.audit ? perms : 0,
 						     5, vec, dfaflags, false))
 			goto fail;
 		count++;
@@ -1023,8 +1029,9 @@ int mnt_rule::gen_policy_re(Profile &prof)
 		if (!convert_entry(mntbuf, mnt_point))
 			goto fail;
 		vec[0] = mntbuf.c_str();
-		if (!prof.policy.rules->add_rule_vec(deny, perms, audit, 1, vec,
-						     dfaflags, false))
+		if (!prof.policy.rules->add_rule_vec(deny, perms,
+					(audit.audit ? perms : 0), 1, vec,
+					dfaflags, false))
 			goto fail;
 		count++;
 	}
@@ -1037,8 +1044,9 @@ int mnt_rule::gen_policy_re(Profile &prof)
 		if (!clear_and_convert_entry(devbuf, device))
 			goto fail;
 		vec[1] = devbuf.c_str();
-		if (!prof.policy.rules->add_rule_vec(deny, perms, audit, 2, vec,
-						     dfaflags, false))
+		if (!prof.policy.rules->add_rule_vec(deny, perms,
+					(audit.audit ? perms : 0), 2, vec,
+					dfaflags, false))
 			goto fail;
 		count++;
 	}

parser/mount.h
@@ -144,7 +144,7 @@ public:
 	std::vector<unsigned int> flagsv, opt_flagsv;
 
 	perms_t perms;
-	int audit;
+	struct { bool audit; } audit;
 	int deny;
 
 	mnt_rule(struct cond_entry *src_conds, char *device_p,

parser/mqueue.cc
@@ -87,7 +87,7 @@ void mqueue_rule::move_conditionals(struct cond_entry *conds)
 }
 
 mqueue_rule::mqueue_rule(perms_t perms_p, struct cond_entry *conds, char *qname_p):
-	qtype(mqueue_unspecified), qname(qname_p), label(NULL), audit(0), deny(0)
+	qtype(mqueue_unspecified), qname(qname_p), label(NULL), audit({false}), deny(0)
 {
 	move_conditionals(conds);
 	free_cond_list(conds);
@@ -115,7 +115,7 @@ mqueue_rule::mqueue_rule(perms_t perms_p, struct cond_entry *conds, char *qname_
 
 ostream &mqueue_rule::dump(ostream &os)
 {
-	if (audit)
+	if (audit.audit)
 		os << "audit ";
 	if (deny)
 		os << "deny ";
@@ -233,10 +233,10 @@ int mqueue_rule::gen_policy_re(Profile &prof)
 			/* store perms at name match so label doesn't need
 			 * to be checked
 			 */
-			if (!label && !prof.policy.rules->add_rule_vec(deny, perms, audit, 1, vec, dfaflags, false))
+			if (!label && !prof.policy.rules->add_rule_vec(deny, perms, audit.audit ? perms : 0, 1, vec, dfaflags, false))
 				goto fail;
 			/* also provide label match with perm */
-			if (!prof.policy.rules->add_rule_vec(deny, perms, audit, size, vec, dfaflags, false))
+			if (!prof.policy.rules->add_rule_vec(deny, perms, audit.audit ? perms : 0, size, vec, dfaflags, false))
 				goto fail;
 		}
 	}
@@ -268,10 +268,10 @@ int mqueue_rule::gen_policy_re(Profile &prof)
 		}
 
 		if (perms & AA_VALID_SYSV_MQ_PERMS) {
-			if (!label && !prof.policy.rules->add_rule_vec(deny, perms, audit, 1, vec, dfaflags, false))
+			if (!label && !prof.policy.rules->add_rule_vec(deny, perms, audit.audit ? perms : 0, 1, vec, dfaflags, false))
 				goto fail;
 			/* also provide label match with perm */
-			if (!prof.policy.rules->add_rule_vec(deny, perms, audit, size, vec, dfaflags, false))
+			if (!prof.policy.rules->add_rule_vec(deny, perms, audit.audit ? perms : 0, size, vec, dfaflags, false))
 				goto fail;
 		}
 	}

commit 210ad63cbf17327811f40250c6fd34c30b19c723

parser: fix encoding of unix permissions for setopt and getopt
The permissions for AA_NET_OPT need to be bounded by mask so we can
make sure it matches when a policy specified only setopt or only
getopt. This was causing failures on the regression tests
unix_socket_pathname, unix_socket_abstract, unix_socket_unnamed and
unix_socket_autobind

Fixes: 44f3be091 ("parser: convert the stored audit from a bit mask to a bool")
Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>

parser/af_unix.cc
@@ -413,7 +413,7 @@ int unix_rule::gen_policy_re(Profile &prof)
 			tmp << "..";
 			buf = tmp.str();
 			if (!prof.policy.rules->add_rule(buf.c_str(), rule_mode == RULE_DENY,
-							 map_perms(AA_NET_OPT),
+							 map_perms(mask & AA_NET_OPT),
 							 map_perms(audit == AUDIT_FORCE ? AA_NET_OPT : 0),
 							 parseopts))
 				goto fail;