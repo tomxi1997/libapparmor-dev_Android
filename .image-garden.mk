1df91e2c8c936a887f34b5bd2d3afcb682b7c72c
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2024-11-26 15:40:19 +0100

    Third iteration of spread support
    - Tests defined in utils/test are now described by a task.yaml in the same
      directory and can run concurrently across many machines.
    - Tests for utils/ are now executed on openSUSE Tumbleweed since ttk themes is
      no longer a hard dependency in master.
    - Tests no longer run on openSUSE Leap 15.6 due to the age of default
      Python (3.6) and gcc/g++. The tight integration with SWIG which does
      not seem to support other Python versions very well. Perl hard-codes
      old GCC for extension modules. The upcoming openSUSE Leap 16 should be
      a viable target. In the meantime we can still test everything through
      rolling-release Tumbleweed.
    - Formatting of YAML files is now more uniform, at four spaces per tab.
    - The run-spread.sh script is now in the root of the tree. The script allows
      running all spread tests sequentially on one system, while collecting logs
      and artifacts for convenient analysis after the fact.
    - All systems are adjusted to run _four_ workers in parallel with _two_ virtual
      cores each and equipped with 1.5GB of virtual memory. This aims to best
      utilize the capacity of a typical CI worker with two to four cores and about
      8GB of available memory.
    - Failing tests are marked as such, so that as a whole the entire spread suite
      can pass and be useful at catching regressions.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- .image-garden.mk
+++ .image-garden.mk
@@ -30,18 +30,13 @@ packages:
 - python3-tk
 - python3-ttkthemes
 - swig
-runcmd:
-- apt clean
 endef
 
 # Ubuntu shares cloud-init profile with Debian.
 UBUNTU_CLOUD_INIT_USER_DATA_TEMPLATE=$(DEBIAN_CLOUD_INIT_USER_DATA_TEMPLATE)
 
-# On openSUSE Leap the default gcc and python are very old. We can use more
-# recent version of Python quite easily but perl extension module system does
-# not want us to modify the CC that's baked into perl and all my attempts at
-# using gcc-14 have failed.
-define OPENSUSE_CLOUD_INIT_USER_DATA_TEMPLATE
+# This is the cloud-init user-data profile for openSUSE Tumbleweed.
+define OPENSUSE_tumbleweed_CLOUD_INIT_USER_DATA_TEMPLATE
 $(CLOUD_INIT_USER_DATA_TEMPLATE)
 packages:
 - attr
@@ -67,12 +62,7 @@ packages:
 - python3-setuptools
 - python3-tk
 - python311
+- python3-devel
 - python311-devel
 - swig
 endef
-
-# On openSUSE tumbleweed the set of packages may drift towards more recent
-# versions more rapidly than on Leap but the moment we want to, for example,
-# move to Python 3.13, we can define a separate entry with different package
-# set or perhaps with $(patsubst)-computed package set.
-OPENSUSE_tumbleweed_CLOUD_INIT_USER_DATA_TEMPLATE=$(OPENSUSE_CLOUD_INIT_USER_DATA_TEMPLATE)
---
+++ run-spread.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Run integration tests with spread sequentially on all the systems, using
+# multiple workers per system. This mode is suitable to run on a single
+# quad-core CPU with 8GB of RAM and no desktop session.
+set -xeu
+
+if test -z "$(command -v spread)"; then
+	echo "You need to install spread from https://github.com/snapcore/spread with the Go compiler and the command: go install github.com/snapcore/spread/cmd/spread@latest" >&2
+	exit 1
+fi
+
+if test -z "$(command -v image-garden)"; then
+	echo "You need to install image-garden from https://gitlab.com/zygoon/image-garden: make install prefix=/usr/local" >&2
+	exit 1
+fi
+
+rm -rf spread-logs spread-artifacts
+mkdir -p spread-logs
+for system in \
+	opensuse-cloud-tumbleweed \
+	debian-cloud-12 \
+	debian-cloud-13 \
+	ubuntu-cloud-22.04 \
+	ubuntu-cloud-24.04 \
+	ubuntu-cloud-24.10; do
+	if ! spread -artifacts ./spread-artifacts -v "$system" | tee spread-logs/"$system".log; then
+		echo "Spread exited with code $?" >spread-logs/"$system".failed
+	fi
+done
