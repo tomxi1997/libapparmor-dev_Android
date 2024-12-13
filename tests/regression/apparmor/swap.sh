9a1b538298664a0d76cc133f75549ed2fd73c479
Grimmauld <Grimmauld@grimmauld.de>
2024-12-13 12:31:19 +0100

    fix swap test on zfs file system

--- tests/regression/apparmor/swap.sh
+++ tests/regression/apparmor/swap.sh
@@ -31,8 +31,8 @@ swap_file=$tmpdir/swapfile
 
 # check if we can run the test in tmpdir
 fstype=$(stat -f --format '%T' "${tmpdir}")
-if [ "${fstype}" = "tmpfs" ] ; then
-	# create a mountpoint not tmpfs
+if [ "${fstype}" = "tmpfs" ] || [ "${fstype}" = "zfs" ] ; then
+	# create a mountpoint not tmpfs or zfs
 	mount_file=$tmpdir/mountfile
 	mount_point=$tmpdir/mountpoint
 	fstype="ext2"
