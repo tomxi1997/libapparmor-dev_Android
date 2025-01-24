9e05668d5a5bb95d9f9f653be0445998fe279092
Ryan Lee <ryan.lee@canonical.com>
2025-01-23 17:11:42 -0800

    Set up an overlayfs_fuse regression test by using the other path of the overlayfs_common.inc helper
    Signed-off-by: Ryan Lee <ryan.lee@canonical.com>

---
+++ tests/regression/apparmor/overlayfs_fuse.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#	Copyright (C) 2024 Canonical, Ltd.
+#
+#	This program is free software; you can redistribute it and/or
+#	modify it under the terms of the GNU General Public License as
+#	published by the Free Software Foundation, version 2 of the
+#	License.
+
+#=NAME overlayfs_fuse
+#=DESCRIPTION
+# Verifies that file rules work in a (fuse) overlayfs
+#=END
+
+source "./overlayfs_common.inc" fuse
