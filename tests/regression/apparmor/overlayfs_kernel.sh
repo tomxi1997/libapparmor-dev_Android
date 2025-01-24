94136582779f3cb80db50e9a0811f05497145ef0
Ryan Lee <ryan.lee@canonical.com>
2025-01-23 16:55:57 -0800

    Move overlayfs test into include helper and wrap in overlayfs_kernel
    By making the test a file to be included as a helper, we can reuse most of the code for a fuse_overlayfs test without copy-pasting
    
    Signed-off-by: Ryan Lee <ryan.lee@canonical.com>

---
+++ tests/regression/apparmor/overlayfs_kernel.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#	Copyright (C) 2024 Canonical, Ltd.
+#
+#	This program is free software; you can redistribute it and/or
+#	modify it under the terms of the GNU General Public License as
+#	published by the Free Software Foundation, version 2 of the
+#	License.
+
+#=NAME overlayfs_kernel
+#=DESCRIPTION
+# Verifies that file rules work in a (kernel) overlayfs
+#=END
+
+source "./overlayfs_common.inc"
